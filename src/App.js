afcd0330a3efe1bd15cce0c5b8d784a21fdbb988
Natsuki <nkondo3@gmail.com>
2020-10-11 23:18:25 +0900

    Changed welcome text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcom to React
         </a>
       </header>
     </div>
